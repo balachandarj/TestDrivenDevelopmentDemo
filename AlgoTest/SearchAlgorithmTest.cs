095eb8357aa70d3e8958d25bb7bb7401dcc37323
balachandarj <balachandarjegan@gmail.com>
2024-03-21 16:10:41 -0700

    Add project files.

---
+++ AlgoTest/SearchAlgorithmTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace AlgoTest
+{
+    [TestFixture]
+    public class SearchAlgorithmTest
+    {
+        [TestCase(new int[] { 1, 4, 5, 15, 20, 25, 30 }, 15, 3)]// Number is in middle
+        [TestCase(new int[] { 1, 4, 5, 15, 20, 25, 30 }, 1, 0)] // Number is in first
+        [TestCase(new int[] { 1, 4, 5, 15, 20, 25, 30 }, 30, 6)] // Number is in last
+        [TestCase(new int[] { 1, 4, 5, 15, 20, 25, 30 }, 16, -1)] // Not exist
+        public void BinarySearchTest(int[] arr, int n, int expectedResult)
+        {
+            var searchAlgorithms = new SearchAlgorithms();
+            var actualResult = searchAlgorithms.BinarySearch(arr, n);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
---
+++ AlgoTest/SearchAlgorithms.cs
@@ -0,0 +1,28 @@
+﻿namespace AlgoTest
+{
+    public class SearchAlgorithms
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="arr">Sorted Array</param>
+        /// <param name="n">Number to find it form an array</param>
+        /// <returns></returns>
+        public int BinarySearch(int[] arr, int n)
+        {
+            int left = 0;
+            int right = arr.Length-1;
+            while(left <= right)
+            {
+                var m = (left+right)/2;
+                if(arr[m]==n)
+                    return m;
+                if(arr[m]>=n)
+                    right=m-1;
+                else
+                    left=m+1;
+            }
+            return -1;
+        }
+    }
+}
